593ed91cd06f5258ee592a7fdf5887cf20b52653
Darren <dartrenbbx@gmail.com>
2020-05-06 21:25:01 -0700

    Learning data structures

---
+++ src/PracticeAndTesting/DataStructures.java
@@ -0,0 +1,10 @@
+package PracticeAndTesting;
+
+public class DataStructures {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/PracticeAndTesting/Testing.java
@@ -0,0 +1,29 @@
+package PracticeAndTesting;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class LinkedList {
+	Node head;
+
+	class Node {
+		int value;
+		Node next;
+
+		Node(int v) {
+			value = v;
+		}
+	}
+
+	static LinkedList insert(LinkedList list, int value) {
+
+		return list;
+
+	}
+}
